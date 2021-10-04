5cdb19d6493ee212a9b2f1fe1df108f387dc1b32
murilonerdx <mu-silva@outlook.com>
2021-10-04 13:01:55 -0300

    Controller

--- book-service/src/main/java/com/murilonerdx/bookservice/controller/BookController.java
+++ book-service/src/main/java/com/murilonerdx/bookservice/controller/BookController.java
@@ -1,6 +1,7 @@
 package com.murilonerdx.bookservice.controller;
 
 import com.murilonerdx.bookservice.model.Book;
+import com.murilonerdx.bookservice.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,13 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
     @Autowired
+    private BookRepository repository;
+
+    @Autowired
     public Environment environment;
 
-    @GetMapping(value="/{id}/")
+    @GetMapping(value="/{id}/{currency}")
     public Book findBook(
         @PathVariable("id") Long id,
         @PathVariable("currency") String currency
     ){
-
+        Book book = repository.getById(id);
+        if(book == null) throw new RuntimeException("Book id "+ id + "not found");
+        String port = environment.getProperty("server.port");
+        book.setEnvironment(port);
+        return book;
     }
 }
