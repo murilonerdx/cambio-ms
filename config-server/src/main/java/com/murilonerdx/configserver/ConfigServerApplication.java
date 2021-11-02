ca9bc3ef642695b3558f83e94a32301897858d5f
murilonerdx <mu-silva@outlook.com>
2021-11-02 19:56:55 -0300

    Spring Cloud API Gateway

--- config-server/src/main/java/com/murilonerdx/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/murilonerdx/configserver/ConfigServerApplication.java
@@ -1,5 +1,6 @@
 package com.murilonerdx.configserver;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
